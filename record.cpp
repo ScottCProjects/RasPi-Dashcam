06d3f6d774cbd9090e7dedd34809c9bd5eb92877
Scott Camarena <scamaren@asu.edu>
2014-04-18 13:01:27 -0700

    Update

--- record.cpp
+++ record.cpp
@@ -13,19 +13,23 @@ int main()
 {
 	int STRSIZE = 256;
 	char str[STRSIZE];
-	unsigned char c;
-
+	char c;
+	
+	cout << "Starting..." << endl;
 	ofstream outfile;
-	outfile.open("testOut.h264", ios::out /*| ios::binary*/);
+	outfile.open("testOut.h264", ios::out | ios::binary);
 	while( cin.good() )
 	{
-		cin.get(str, STRSIZE);
+		//cin.get(str, STRSIZE);
+		c = cin.get();
 		if( cin.good() )
 			//cout.put(c);
-			outfile.write(str, STRSIZE-1);
+			//outfile.write(str, STRSIZE-1);
+			outfile.put(c);
 	}
 
 	outfile.close();
-
+	
+	cout << "Done." << endl;
 	return 0;
 }
