dc8d04f5791e9a52811f7a1d999461fa6937e379
Scott Camarena <scott.camarena@gmail.com>
2014-04-22 18:48:06 -0700

    Modified record

--- record.cpp
+++ record.cpp
@@ -6,8 +6,24 @@
 
 #include <iostream>
 #include <fstream>
+#include <thread>
 
 using namespace std;
+
+void openFile( ofstream &f, string filename )
+{
+	f.open( filename, ios::out );
+}
+
+void closeFile( ofstream &f )
+{
+	f.close();
+}
+
+string filename( string prefix, int num )
+{
+	return prefix + string( itoa(num) ) + ".h264";
+}
 
 int main()
 {
@@ -16,20 +32,30 @@ int main()
 	char c;
 	
 	cout << "Starting..." << endl;
-	ofstream outfile;
-	outfile.open("testOut.h264", ios::out | ios::binary);
-	while( cin.good() )
+	ofstream output[2];
+	string fnamePrefix = "test";
+	int numFiles = 4;
+	int fileNum = 1;
+
+	// Open up the first file
+	thread( openFile, output[0], filename(fnamePrefix, fileNum) );
+	/*
+	// Loop
+	for( int i = 0, out = 0; i < 10; ++i )
 	{
-		//cin.get(str, STRSIZE);
-		c = cin.get();
+		// Set filename
+		if( ++fileNum > 4 ) fileNum = 1;
 		if( cin.good() )
-			//cout.put(c);
-			//outfile.write(str, STRSIZE-1);
+			c = cin.get();
+		while( cin.good() )
+		{
 			outfile.put(c);
+			c = cin.get();
+		}
 	}
 
 	outfile.close();
-	
+	*/
 	cout << "Done." << endl;
 	return 0;
 }
