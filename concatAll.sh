9955339db06e776ac4d3bed0867fcd7c7a310978
Scott Camarena <scamaren@asu.edu>
2014-04-23 09:58:06 -0700

    Added concat

---
+++ concatAll.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+prefix=$1
+numFiles=$2
+startNum=$3
+
+outFile="$prefix"All.h264
+
+echo Output File: $outFile
+
+cat /dev/null > $outFile
+
+declare -i i
+i=$startNum
+((++i))
+
+while [ $i -le $numFiles ]
+do
+	cat $prefix$i.h264 >> $outFile
+	((++i))
+done
+
+i=1
+while [ $i -lt $startNum ]
+do
+	cat $prefix$i.h264 >> $outFile
+	((++i))
+done
